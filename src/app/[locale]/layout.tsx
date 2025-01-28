c6b4afde9c9c8dae38c539ad6e6f311995945fdb
Dmytro Pechunka <pechunka11@gmail.com>
2025-01-28 22:45:28 +0100

    add pixel

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -73,11 +73,31 @@ export default async function RootLayout({
               r.appendChild(e);
           })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 5284129);`}
         </Script>
+        <Script id={'Meta pixel'}>
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1301568837652755');
+            fbq('track', 'PageView');`}
+        </Script>
       </head>
       {/* <!-- Google tag (gtag.js) --> */}
 
       <GoogleAnalytics gaId={'G-6BLR9FQVT3'} />
       <body className={`bg-base antialiased overflow-x-hidden`}>
+        <noscript>
+          <img
+            height={'1'}
+            width={'1'}
+            style={{ display: 'none' }}
+            src={'https://www.facebook.com/tr?id=1301568837652755&ev=PageView&noscript=1'}
+          />
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           <Header dataNav={dataNav} linkReserve={dataContact.linkToReserve} />
           {children}
