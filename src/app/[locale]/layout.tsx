869c8bfb2e7c3dd06d3fcf09992e4e70f6bfeaf8
Dmytro Pechunka <pechunka11@gmail.com>
2025-02-20 10:16:38 +0100

    remove Clarity code, add from GTM

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -55,13 +55,6 @@ export default async function RootLayout({
         <link rel={'shortcut icon'} href={'/favicon/favicon.ico'} />
         <link rel={'apple-touch-icon'} sizes={'180x180'} href={'/favicon/apple-touch-icon.png'} />
         <link rel={'manifest'} href={'/favicon/site.webmanifest'} />
-        <Script id={'Clarity'}>
-          {`(function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "qcpno3jbtb");`}
-        </Script>
         <Script id={'Meta pixel'}>
           {`!function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
