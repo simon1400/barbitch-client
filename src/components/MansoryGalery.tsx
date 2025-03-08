852a47e16217fb0d14a48586b6961c8e59719692
Dmytro Pechunka <pechunka11@gmail.com>
2025-03-08 11:15:09 +0100

    fixed galery

--- src/components/MansoryGalery.tsx
+++ src/components/MansoryGalery.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { motion } from 'motion/react'
 import { CldImage } from 'next-cloudinary'
 import dynamic from 'next/dynamic'
 import { useState } from 'react'
@@ -27,38 +26,23 @@ export const MasonryGalery = ({ images }: MasonryGalleryProps) => {
       <h2 id={'masonry-gallery'} className={'sr-only'}>
         {'Masonry Gallery'}
       </h2>
-      <motion.div
-        initial={{ x: 0 }}
-        animate={{
-          x: '-66.6%',
-          transition: {
-            ease: 'linear',
-            duration: 130,
-            repeat: Infinity,
-          },
-        }}
-        className={`flex gap-'3' : '5'} w-[300%] relative`}
-      >
-        {[...Array.from({ length: 3 })].map((_, index) => (
-          <MasonryGrid key={index} className={'mx-3'}>
-            {images.map((image, idx) => (
-              <div
-                key={`${image.hash}-${index}`}
-                className={'relative cursor-pointer'}
-                onClick={() => setIndex(idx)}
-              >
-                <CldImage
-                  src={image.hash}
-                  width={300}
-                  height={400}
-                  alt={`Obrázek ${idx + 1} z galerie: ${image.name}`}
-                  loading={'lazy'} // Lazy loading для производительности
-                />
-              </div>
-            ))}
-          </MasonryGrid>
+      <MasonryGrid key={index} className={'mx-3'}>
+        {images.map((image, idx) => (
+          <div
+            key={`${image.hash}-${index}`}
+            className={'relative cursor-pointer'}
+            onClick={() => setIndex(idx)}
+          >
+            <CldImage
+              src={image.hash}
+              width={600}
+              height={600}
+              alt={`Obrázek ${idx + 1} z galerie: ${image.name}`}
+              loading={'lazy'} // Lazy loading для производительности
+            />
+          </div>
         ))}
-      </motion.div>
+      </MasonryGrid>
       {/* Lightbox загружается только при клике */}
       {index >= 0 && (
         <Lightbox
