76c78c04a81f5f9b308d122af204d1c417837ef0
Dmytro Pechunka <pechunka11@gmail.com>
2025-06-24 10:49:54 +0200

    fixed galery

--- src/components/dynamicComponents/Galery.tsx
+++ src/components/dynamicComponents/Galery.tsx
@@ -10,17 +10,17 @@ export const Galery = ({
 }) => {
   const length = data.image.length
   return (
-    <section className={'pt-0 pb-12 md:pt-10 mb:pb-17'}>
+    <section className={'pt-0 pb-12 mb:pb-17'}>
       <Container size={'xl'}>
         <div
-          className={`grid grid-cols-2 md:grid-cols-${length >= 3 ? '3' : length} gap-4 md:gap-8`}
+          className={`grid grid-cols-${length > 1 ? '2' : length} md:grid-cols-${length >= 3 ? '3' : length} gap-4 md:gap-8`}
         >
           {data.image.map((item) => (
             <div key={item.documentId} className={`${!contain && 'pt-[100%]'} relative`}>
               <Image
                 className={`${contain ? 'object-contain' : 'object-cover'} object-center`}
                 src={item.url}
-                width={700}
+                width={length > 1 ? 700 : 1400}
                 height={1000}
                 fill={!contain}
                 alt={item.alternativeText || ''}
