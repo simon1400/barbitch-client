f285a4064b1cae3719a299e5f35be609fa941bd1
Dmytro Pechunka <pechunka11@gmail.com>
2025-07-06 16:46:16 +0200

    add new chatbox

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,19 @@ export default async function RootLayout({
           <Banner />
           <Footer />
         </AppProvider>
+        {/* <!--Start of Tawk.to Script--> */}
+        <Script>
+          {`var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/686a45cce82cfa190ecdc718/1ivfi1aam';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();`}
+        </Script>
+        {/* <!--End of Tawk.to Script--> */}
       </body>
     </html>
   )
