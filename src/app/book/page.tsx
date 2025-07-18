7d2266a87277aba12d9cbb9f0016ecb69bcc34e8
Dmytro Pechunka <pechunka11@gmail.com>
2025-07-18 12:20:46 +0200

    add chose master step

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -53,7 +53,7 @@ const BookServiceItem = ({ service }: { service: IBookService }) => {
     <li className={'border-t-2 border-[#3C3C3C] border-dotted'}>
       <Link
         className={'flex py-3.5 px-1 items-center gap-4 hover:bg-[#3C3C3C] duration-200'}
-        href={`/book/${service.id}/any`}
+        href={`/book/${service.id}`}
       >
         <span
           className={'min-w-[36px] w-[36px] h-[36px] overflow-hidden rounded-full block relative'}
