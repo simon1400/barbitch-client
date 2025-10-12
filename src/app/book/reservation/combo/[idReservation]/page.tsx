5bed7f1a54a4a216bbfc9437c49ad8d26a134ec2
Dmytro Pechunka <pechunka11@gmail.com>
2025-10-12 15:09:57 +0200

    fixed type error

--- src/app/book/reservation/combo/[idReservation]/page.tsx
+++ src/app/book/reservation/combo/[idReservation]/page.tsx
@@ -1,7 +1,5 @@
 import ComboBookForm from './ComboBookForm'
 
 export default async function ComboBookServicePage({ params }: any) {
-  const { idReservation } = await params
-
-  return <ComboBookForm idReservation={idReservation} />
+  return <ComboBookForm />
 }
