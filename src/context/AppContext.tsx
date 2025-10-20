f666de18239d5cfc1191cb352a9dbbeeddd63825
Dmytro Pechunka <pechunka11@gmail.com>
2025-10-20 19:48:11 +0200

    fixed split admin module to separe app

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,18 +1,11 @@
 /* eslint-disable react/no-unstable-context-value */
 'use client'
 
-import type { UserRole } from 'app/admin/data'
 import type { ReactNode } from 'react'
 
 import { createContext, useContext, useState } from 'react'
 
 interface AppContextType {
-  adminName: string
-  setAdminName: (value: string) => void
-  userRole: UserRole | null
-  setUserRole: (value: UserRole | null) => void
-  select: string
-  setSelect: (value: string) => void
   menu: boolean
   setMenu: (value: boolean) => void
 }
@@ -20,18 +13,9 @@ interface AppContextType {
 const AppContext = createContext<AppContextType | undefined>(undefined)
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
-  const [adminName, setAdminName] = useState<string>('')
-  const [userRole, setUserRole] = useState<UserRole | null>(null)
   const [menu, setMenu] = useState<boolean>(false)
-  const [select, setSelect] = useState<string>('works')
 
   const adminValues = {
-    adminName,
-    setAdminName,
-    userRole,
-    setUserRole,
-    select,
-    setSelect,
     menu,
     setMenu,
   }
