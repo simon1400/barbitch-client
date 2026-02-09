6c6ddb7d1a4071a5ea4285f6df69d2f2c716aecd
Dmytro Pechunka <pechunka11@gmail.com>
2026-02-09 23:06:16 +0100

    fixed style footer bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,8 @@ import { withHiddenRoutes } from 'helpers/withHiddenRoutes'
 // import dynamic from 'next/dynamic'
 
 import Contact from '../sections/Contact'
+
+import { Container } from './Container'
 // const CustomMap = dynamic(() => import('../sections/Map'))
 
 const Footer = async () => {
@@ -9,6 +11,28 @@ const Footer = async () => {
     <footer className={'pt-27 lg:pt-30'}>
       <Contact />
       {/* <CustomMap /> */}
+      <section className={'bg-accent text-white '}>
+        <Container size={'xl'}>
+          <div className={'flex items-center justify-between py-4'}>
+            <div>
+              <a className={'hover:text-primary'} href={'/zasady-ochrany-osobnich-udaju'}>
+                {'Zásady ochrany osobních údajů'}
+              </a>
+            </div>
+            <div>
+              <p>{'Provozovatel: Dmytro Pechunka, IČO: 17407613'}</p>
+            </div>
+            <div className={'flex gap-5'}>
+              <a className={'hover:text-primary'} href={'/obchodni-podminky'}>
+                {'Obchodní podmínky'}
+              </a>
+              <a className={'hover:text-primary'} href={'/cookies-policy'}>
+                {'Cookies Policy'}
+              </a>
+            </div>
+          </div>
+        </Container>
+      </section>
     </footer>
   )
 }
